b228046793320f18f9bb4c2a3e51e92a06b3dae6
Salim Tufts <salimtufts9@gmail.com>
2026-07-26 18:33:24 -0700

    Added switch cases for calculation options

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,7 +1,11 @@
 const numberButtons = document.querySelector("#numbers");
 const operatorButtons = document.querySelector("#operators");
 const output = document.querySelector("#output");
-const clearBtn = document.querySelector("#clear");
+
+let first = "";
+let second = "";
+let operation = "";
+let preCalc = true;
 
 // Create Buttons
 for (let i = 0; i <= 9; i++){
@@ -14,9 +18,85 @@ for (let i = 0; i <= 9; i++){
 // Add event listener to each button
 const buttons = document.querySelectorAll("button");
 buttons.forEach(btn => btn.addEventListener("click", () => {
-  output.textContent += btn.id;
+  if (preCalc) {
+    switch(btn.id) {
+      case "clear":
+        output.textContent = "";
+        first = "";
+        second = "";
+        operator = "";
+        preCalc = true;
+        break;
+      case "+":
+      case "-":
+      case "/":
+      case "x":
+        output.textContent = btn.id;
+        operation = btn.id;
+        preCalc = false;
+        break;
+      case "=":
+        output.textContent = first;
+        break;
+      default:
+        output.textContent += btn.id;
+        first += btn.id;
+        break;
+    }
+  } else {
+    switch(btn.id) {
+      case "clear":
+        output.textContent = "";
+        first = "";
+        second = "";
+        operation = "";
+        preCalc = true;
+        break;
+      case "+":
+      case "-":
+      case "/":
+      case "x":
+      case "=":
+        if (operation == "+"){
+          first = add(first, second);
+        } else if (operation == "-") {
+          first = subtract(first, second);
+        } else if (operation == "/") {
+          first = divide(first, second);
+        } else {
+          first = multiply(first, second);
+        }
+        second = "";
+        break;
+      default:
+        output.textContent = btn.id;
+        second += btn.id;
+        break;
+    }
+  }
 }));
 
-clearBtn.addEventListener("click", () => {
-  output.textContent = "";
-});
+
+// Math Operation Functions
+
+function add(a, b) {
+  a = Number(a);
+  b = Number(b);
+  output.textContent = a + b;
+};
+function subtract(a, b) {
+  a = Number(a);
+  b = Number(b);
+  output.textContent = a - b;
+};
+function divide(a, b) {
+  a = Number(a);
+  b = Number(b);
+  output.textContent = a / b;
+};
+function multiply(a, b) {
+  a = Number(a);
+  b = Number(b);
+  output.textContent = a * b;
+};
+
